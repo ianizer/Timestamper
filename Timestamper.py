f6b681cfd7f7f754ebae6115c4043cfc705e5ce9
Ianizer <87508438+ianizer@users.noreply.github.com>
2026-04-30 23:48:24 -0500

    Populate a CTk ComboBox with years (experimental).

--- Timestamper.py
+++ Timestamper.py
@@ -1,6 +1,18 @@
-import datetime
+from datetime import datetime
 import pyperclip  # for clipboard functions.
 import customtkinter as ctk
+
+def list_years():
+    numYears = datetime.now().year - datetime.fromtimestamp(0).year # issue: if current date is before epoch, then negative value occurs.  however this doesn't matter because times before the epoch are impossible to display on discord, without utilizing integer overflow.
+
+    year_list = []
+
+    for i in range(numYears + 50):
+        year_list.append((datetime.fromtimestamp(0).year + i).__str__())
+    
+    return year_list
+        
+        
 
 class Timestamper:
     def __init__(self):
@@ -17,18 +29,21 @@ class Timestamper:
         # Weight=1 = fill all extra space from resizing. 
         # If a different row has a different number (say weight=2), then twice as much free space will be taken by the row with weight=2 as the row with weight=1.
         # Weight=0 = no filling extra space.
-        self.gui.grid_rowconfigure(0, weight=1)
+        # self.gui.grid_rowconfigure(0, weight=1)
 
         self.entry_box = ctk.CTkEntry(self.gui, placeholder_text="Enter date", width=200, height=40) # make it exist
         self.entry_box.grid(row=0, column=0, sticky="ew", padx=50, pady=10) # place on grid (make visible)
 
-        self.copy_button = ctk.CTkButton(self.gui, text="Copy to Clipboard", command=self.bt_copy_to_clipboard)
-        self.copy_button.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
+        self.year_input = ctk.CTkComboBox(self.gui, values=list_years())
+        self.year_input.grid(row=1, column=0, sticky="w", padx=50, pady=10) # place on grid (make visible)
 
+        self.copy_button = ctk.CTkButton(self.gui, text="Copy to Clipboard", command=self.bt_copy_to_clipboard)
+        self.copy_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
+        
         self.gui.mainloop()
 
     def bt_copy_to_clipboard(self):
-        chosen_date = datetime.datetime.strptime(self.entry_box.get(), "%Y-%m-%d %H:%M:%S")
+        chosen_date = datetime.strptime(self.entry_box.get(), "%Y-%m-%d %H:%M:%S")
         unix_timestamp = int(chosen_date.timestamp())
         formatted_timestamp = f"<t:{unix_timestamp}:R>"
 
@@ -41,5 +56,5 @@ class Timestamper:
         # current rough idea: convert date string from input box into a datetime object, then call .timestamp(), then add formatting based on mode selected from gui (:R, :F, etc)
 
 
-
+print(datetime.fromtimestamp(0))
 program = Timestamper()
