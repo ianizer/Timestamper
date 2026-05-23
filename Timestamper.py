5cf9e1f56bdbb36fba403e6cd1ad0ce333965206
ianizer <87508438+ianizer@users.noreply.github.com>
2026-05-23 15:35:18 -0500

    Add a FloatSpinbox from CTk documentation.

--- Timestamper.py
+++ Timestamper.py
@@ -1,6 +1,7 @@
 from datetime import datetime  # Self-explanatory.
 import pyperclip  # Clipboard functions.
 import customtkinter as ctk  # GUI.
+from typing import Callable
 
 
 # This function might be deleted later.
@@ -13,6 +14,87 @@ def list_years():
     year_list = [str(datetime.fromtimestamp(0).year + i) for i in range(numYears + 50)]
 
     return year_list
+
+
+# Example code from CTk docs.
+class FloatSpinbox(ctk.CTkFrame):
+    def __init__(
+        self,
+        *args,
+        width: int = 100,
+        height: int = 32,
+        step_size: int | float = 1,
+        command: Callable | None = None,
+        **kwargs,
+    ):
+        super().__init__(*args, width=width, height=height, **kwargs)
+
+        self.step_size = step_size
+        self.command = command
+
+        self.configure(fg_color=("gray78", "gray28"))  # set frame color
+
+        self.grid_columnconfigure((0, 2), weight=0)  # buttons don't expand
+        self.grid_columnconfigure(1, weight=1)  # entry expands
+
+        self.subtract_button = ctk.CTkButton(
+            self,
+            text="-",
+            width=height - 6,
+            height=height - 6,
+            command=self.subtract_button_callback,
+        )
+        self.subtract_button.grid(row=0, column=0, padx=(3, 0), pady=3)
+
+        self.entry = ctk.CTkEntry(
+            self, width=width - (2 * height), height=height - 6, border_width=0
+        )
+        self.entry.grid(row=0, column=1, columnspan=1, padx=3, pady=3, sticky="ew")
+
+        self.add_button = ctk.CTkButton(
+            self,
+            text="+",
+            width=height - 6,
+            height=height - 6,
+            command=self.add_button_callback,
+        )
+        self.add_button.grid(row=0, column=2, padx=(0, 3), pady=3)
+
+        # default value
+        self.entry.insert(0, "0.0")
+
+    def add_button_callback(self):
+        if self.command is not None:
+            self.command()
+        try:
+            value = float(self.entry.get()) + self.step_size
+            self.entry.delete(0, "end")
+            self.entry.insert(0, value)
+        except ValueError:
+            return
+
+    def subtract_button_callback(self):
+        if self.command is not None:
+            self.command()
+        try:
+            value = float(self.entry.get()) - self.step_size
+            self.entry.delete(0, "end")
+            self.entry.insert(0, value)
+        except ValueError:
+            return
+
+    def get(self) -> float | None:
+        try:
+            return float(self.entry.get())
+        except ValueError:
+            return None
+
+    def set(self, value: float):
+        self.entry.delete(0, "end")
+        self.entry.insert(0, str(float(value)))
+
+
+# END CTk docs example
 
 
 class TimestamperApp(ctk.CTk):
@@ -78,6 +160,9 @@ class TimestamperApp(ctk.CTk):
             pady=10,
             sticky="nsew",
         )
+
+        self.test_spinbox = FloatSpinbox(self)
+        self.test_spinbox.grid(row=3, column=0, sticky="nsew")
 
         self.mainloop()
 
